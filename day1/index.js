e96d42c3753879b06be4446546aaada7c550c1ce
AbhishekPanta8 <72380871+AbhishekPanta8@users.noreply.github.com>
2021-06-08 14:28:35 +0530

    Update index.js

--- day1/index.js
+++ day1/index.js
@@ -1,2 +1,46 @@
-//showing the message
-alert("This is message");
+/* //1.showing the message
+alert("This is message"); */
+
+/* //2.Working with variables
+let admin,name;
+name="john";
+admin = name;
+alert("admin"); */
+
+/* //3.Giving the right name
+let planet,name;
+planet="Earth";
+name="Abhishek"; */
+
+/* //4.string quotes
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya */
+
+/* //5.Tasks
+let name;
+name= prompt("what is your name?");
+alert(name); */
+
+/* //6.The name of Javascript
+let nameJs = prompt("what is the official name of javscript?");
+alert((nameJs=="ECMAScript")? "Right": "You don't know? ECMAScript!"); */
+
+/* //7.Show a sign
+let num = prompt("Enter a number!");
+if(num>1)alert(1);
+else if(num<1) alert(-1);
+else alert(0); */
+
+/* //8.convert to ?
+let result = (a + b < 4) ? 'Below' : 'Over'; */
+
+/* //9.convert to ?
+let message = (login == 'Employee') ? 'Hello' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' :
+  ''; */
