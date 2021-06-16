5ca3e9c7584bb29506df1236d8837524ce9bf44f
AbhishekPanta8 <72380871+AbhishekPanta8@users.noreply.github.com>
2021-06-16 15:29:03 +0530

    Add files via upload

---
+++ practice/ClassA/classExtendsobj.js
@@ -0,0 +1,11 @@
+//static properites task
+class Rabbit extends Object {
+  constructor(name) {
+    super(); 
+    this.name = name;
+  }
+}
+
+let rabbit = new Rabbit("Rab");
+
+console.log( rabbit.hasOwnProperty('name') );
---
+++ practice/ClassA/test.js
@@ -0,0 +1,14 @@
+class Article {
+  constructor(title, date) {
+    this.title = title;
+    this.date = date;
+  }
+
+   static createTodays() {
+    // remember, this = Article
+    return new this("Today's digest", new Date());
+  }
+}
+
+let article = Article.createTodays();
+console.log( article.title );
